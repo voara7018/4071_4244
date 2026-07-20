89c22a8bc4824bb78d192245ce1bb5037c53fdcc
Lofo-hub <lofoniaina1325@gmail.com>
2026-07-20 15:54:14 +0300

    sd

--- app/Views/prefixes.php
+++ app/Views/prefixes.php
@@ -6,6 +6,13 @@
     <title>Ajouter prefixes</title>
 </head>
 <body>
+        <h1>Liste des prefixes</h1>
+        <ul>
+            <?php foreach ($prefixes as $prefix): ?>
+                <li><?= esc($prefix['prefixes']) ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
     <form action="<?= base_url('prefixes/insert') ?>" method="post">
         <input type="text" name="prefixes" placeholder="Entrez le prefixes" required>
         <button type="submit">Ajouter le prefixes </button>
