ec447a0553e530bc2486f5bf13a85b122a8fede4
Lofo-hub <lofoniaina1325@gmail.com>
2026-07-20 13:05:57 +0300

    pref

---
+++ app/Controllers/PrefixeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+use App\Models\PrefixesModel;
+use Exception;
+
+
+class PrefixeController extends BaseController
+{
+    public function index(): string
+    {
+        return view('admin/prefixes');
+    }
+    public function insert(){
+        $prefixesModel = new PrefixesModel();
+        $prefixes = $this->request->getPost('prefixes');
+        $data = [
+            'prefixes' => $prefixes,
+            'statut' => 1
+        ];
+        try {
+            $prefixesModel->insert($data);
+            return redirect()->to('/prefixes')->with('success', 'Le préfixe a été ajouté avec succès.');
+        } catch (Exception $e) {
+            return redirect()->to('/prefixes')->with('error', 'Une erreur est survenue lors de l\'ajout du préfixe : ' . $e->getMessage()); 
+    }
+    }   
+}
